059ac20e6c54a61a569f52d6878ff41c56e84670
Cyraullie <cyril.goldenschue@gmail.com>
2026-03-11 18:25:31 +0100

    change db connection

--- lib/db.ts
+++ lib/db.ts
@@ -4,5 +4,6 @@ export const db = new Pool({
   connectionString: process.env.library_pi_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  options: "-c search_path=library_pi"
 });
